4c613d1aa15eaacfb581aa624aad46bdeeacebe3
lishirong@badousoft.com <lishirong@badousoft.com>
2020-12-07 19:24:03 +0800

    对接商品管理接口

--- src/main.js
+++ src/main.js
@@ -5,9 +5,19 @@ import '@/plugins/element.js'
 import 'element-ui/lib/theme-chalk/index.css';
 import 'element-ui/lib/index'
 import '@/assets/font/iconfont.css'
-
+import axios from 'axios'
+import Qs from 'qs'
+Vue.prototype.$http = axios
+Vue.prototype.$http.interceptors.request.use(function (config) {
+  if (config.data) {
+    config.data = Qs.stringify(config.data)
+    return config
+  }
+})
+axios.defaults.baseURL = 'http://localhost:8080'
 // 引入全局通用样式
 import '@/assets/css/global.css'
+
 
 Vue.config.productionTip = false
 
--- src/views/product/List.vue
+++ src/views/product/List.vue
@@ -1,67 +1,80 @@
 <template>
-  <div class="main_list">
-    <el-form :inline="true" :model="formInline" class="demo-form-inline search_form">
-      <el-form-item>
-        <el-input v-model="formInline.user" placeholder="请输入商品名称"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-select v-model="formInline.region" placeholder="请选择平台类型">
-          <el-option label="天猫" value="1"></el-option>
-          <el-option label="淘宝" value="2"></el-option>
-          <el-option label="拼多多" value="3"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="main_content" ref="mainContent" :model="searchForm">
+    <div class="main_content" ref="searchForm">
+      <el-form :inline="true" class="demo-form-inline search_form">
+        <el-form-item>
+          <el-input v-model="searchForm.title" placeholder="请输入商品名称"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="searchForm.shopName" placeholder="请输入店铺名称"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-select v-model="searchForm.appType" placeholder="请选择平台类型">
+            <el-option label="全部" value=""></el-option>
+            <el-option label="天猫" value="1"></el-option>
+            <el-option label="淘宝" value="2"></el-option>
+            <el-option label="拼多多" value="3"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="search">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
 
     <!-- 列表 -->
     <div class="list_content">
-      <div class="button_list">
+      <div class="button_list" ref="buttonList">
         <!-- 按钮 -->
-        <el-button>新增</el-button>
-        <el-button>修改</el-button>
-        <el-button>删除</el-button>
+        <el-button type="success" class="el-icon-plus">新增</el-button>
+        <el-button type="primary" class="el-icon-edit">修改</el-button>
+        <el-button type="danger" class="el-icon-delete">删除</el-button>
       </div>
 
       <!-- 表格 -->
-      <el-table
-        :height="tableHeight"
-        ref="multipleTable"
+      <el-table class="table_list"
+        ref="tableList"
         :data="tableData"
         tooltip-effect="dark"
         @selection-change="handleSelectionChange"
-        v-setHeight="{tableHeight:tableHeight}">
-        <el-table-column class="table_column_field"
-                         type="selection">
-        </el-table-column>
+        :height="tableHeight">
+        <el-table-column class="table_column_field" type="selection"></el-table-column>
         <el-table-column
-          label="日期">
+          label="商品平台">
           <template slot-scope="scope">{{ scope.row.date }}</template>
         </el-table-column>
         <el-table-column
+          prop="address"
+          label="店铺"
+          show-overflow-tooltip>
+        </el-table-column>
+        <el-table-column
           prop="name"
-          label="姓名">
+          label="商品名称">
         </el-table-column>
         <el-table-column
           prop="address"
-          label="地址"
+          label="商品分类"
+          show-overflow-tooltip>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="活动截止时间"
           show-overflow-tooltip>
         </el-table-column>
       </el-table>
 
       <!-- 分页 -->
-      <div class="list_pagination block">
+      <div class="table_pagination block" ref="pagination">
         <el-pagination
           background
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="curPage"
-          :page-sizes="[100, 200, 300, 400]"
-          :page-size="100"
+          @size-change="search"
+          @current-change="search"
+          :current-page="page.curPage"
+          :page-sizes="page.sizes"
+          :page-size="page.size"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="400">
+          :total="page.totalSize">
         </el-pagination>
       </div>
     </div>
@@ -70,21 +83,20 @@
 
 <script>
 export default {
-  directives: {
-    setHeight: function (el, binding, vnode) {
-      setTimeout(function() {
-        vnode.context.tableHeight = 400
-      })
-    }
-  },
   data() {
     return {
-      tableHeight: 300,
-      formInline: {
-        user: '',
-        region: ''
+      tableHeight: 0,
+      page: {
+        curPage: 1, // 当前页
+        sizes: [10, 20, 50, 100], // 默认可查询分页数
+        totalSize: 100, // 总记录数
+        size: 10, // 默认显示为10条
       },
-      curPage: 4,
+      searchForm: {
+        title: '',
+        shopName: '',
+        appType: ''
+      },
       tableData: [{
         date: '2016-05-03',
         name: '王小虎',
@@ -127,7 +139,6 @@ export default {
       multipleSelection: []
     }
   },
-
   methods: {
     toggleSelection(rows) {
       if (rows) {
@@ -141,23 +152,46 @@ export default {
     handleSelectionChange(val) {
       this.multipleSelection = val;
     },
-    onSubmit() {
-      console.log('submit!');
-    },
-    handleCurrentChange() {
-
-    },
-    handleSizeChange() {
-
+    // 分页查询
+    search() {
+      let pageObj = this.page // 获取当前分页数据
+      let searchForm = this.searchForm // 查询搜索参数
+      let data = {
+        page: pageObj.curPage,
+        psize: pageObj.size,
+        productName: searchForm.title,
+        appType: searchForm.appType,
+        shopName: searchForm.shopName
+      }
+      this.$http.post("/product/list", data).then((res) => {
+        // debugger
+      })
     }
+  },
+  mounted () {
+    // 查询商品信息
+    this.search()
+  },
+  updated () {
+    // 自适应table内容高度
+    let totalHeight = document.documentElement.clientHeight
+    let contentPadding = 30
+    let tableHeader = 48
+    let searchFormHeight = this.$refs.searchForm.offsetHeight + 20
+    let buttomListHeight = this.$refs.buttonList.offsetHeight
+    let paginationHeight = this.$refs.pagination.offsetHeight
+    this.tableHeight = totalHeight - searchFormHeight - buttomListHeight - paginationHeight - contentPadding - tableHeader
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .main_list {
+  /* 主内容 */
+  .main_content {
     width: auto;
     box-sizing: border-box;
+    height: 60px;
+    z-index: -1;
   }
 
   /* 搜索框 */
@@ -169,6 +203,7 @@ export default {
     justify-content: left;
     padding: 10px;
     margin: 10px;
+    z-index: 1;
   }
 
   .el-form-item {
@@ -188,6 +223,23 @@ export default {
 
     .list_pagination {
       padding: 15px;
+    }
+
+    button {
+      padding: 12px 15px;
+    }
+
+    button::before {
+      padding-right: 5px;
+    }
+  }
+
+  /* 分页 */
+  .table_pagination {
+    padding: 15px 0;
+
+    .el-pagination {
+      font-weight: 400;
     }
   }
 </style>
